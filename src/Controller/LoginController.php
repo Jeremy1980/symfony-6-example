a39984ba6bdee00e0ab1f68fe73b5fb4eaf7ecde
Jarema <jeremy.cz@wp.pl>
2023-12-27 09:03:12 +0100

    Registration; Reset Password; some clean-up

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/autoryzacja', name: 'app_login')]
+    #[Route('/uwierzytelnianie', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
          // get the login error if there is one
