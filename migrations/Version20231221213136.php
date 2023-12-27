c1a2856760400066c6c49d814d86ac5ecaed4325
Jeremy <jeremy.cz@wp.pl>
2023-12-27 14:33:30 +0100

    Fix migration

--- migrations/Version20231221213136.php
+++ migrations/Version20231221213136.php
@@ -22,18 +22,16 @@ final class Version20231221213136 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
-        $this->addSql('ALTER TABLE posts DROP FOREIGN KEY posts_ibfk_1');
-        $this->addSql('DROP INDEX user_id ON posts');
-        $this->addSql('ALTER TABLE user CHANGE is_verified is_verified TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE `user` ADD `is_verified` TINYINT(1) NOT NULL AFTER `password`');
+
+        $this->addSql('CREATE TABLE `messenger_messages` ( `id` bigint AUTO_INCREMENT NOT NULL, `body` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, `queue_name` varchar(190) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL, `created_at` datetime NOT NULL, `available_at` datetime NOT NULL, `delivered_at` datetime DEFAULT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;');
+        $this->addSql('ALTER TABLE `messenger_messages` ADD PRIMARY KEY (`id`)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('DROP TABLE reset_password_request');
-        $this->addSql('ALTER TABLE `user` CHANGE is_verified is_verified INT NOT NULL');
-        $this->addSql('ALTER TABLE `posts` ADD CONSTRAINT posts_ibfk_1 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION');
-        $this->addSql('CREATE INDEX user_id ON `posts` (user_id)');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
